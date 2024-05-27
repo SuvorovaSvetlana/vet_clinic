c466eae2b88693533b1547d8dca3e9c7a6585031
Suvorova Svetlana <suvorova.brs@gmail.com>
2024-05-27 15:23:21 +0200

    add Link.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
+import Link from './components/Link'
 
 function App() {
-      
+      return <div>
+            <Link to="/buttonPage">Go to Button page</Link>
+            <Link to="/dropdownPage">Go to Dropdown page</Link>
+      </div>
 }
 export default App;
