760da5adcd4ce947bed971f3fa3332eb6187a59e
Suvorova Svetlana <suvorova.brs@gmail.com>
2024-05-28 13:49:58 +0200

    add home page sidebar logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,19 @@
-import Link from './components/Link';
+import Sidebar from './components/Sidebar';
 import Route from './components/Route';
 import ButtonPage from './pages/ButtonPage';
 import DropdownPage from './pages/DropdownPage';
 
 function App() {
-      return <div>
-            <Link to="/buttonPage">Go to Button page</Link>
-            <Link to="/dropdownPage">Go to Dropdown page</Link>
-            <div>
+      return (<div className='container mx-auto grid grid-cols-6 gap-4 mt-4'>
+                <Sidebar/>  
+            <div className='col-span-5'>
                   <Route path='/buttonPage'>
                         <ButtonPage/>
                   </Route>
-                  <Route path="/dropdownPage">
+                  <Route path="/">
                         <DropdownPage/>
                   </Route>
             </div>
-      </div>
+      </div>)
 }
 export default App;
---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,28 @@
+import Link from "./Link";
+
+function Sidebar (){
+      const links = [
+            {label: 'Dropdown', path: '/'},
+            {label: 'Button', path: '/buttonPage'},
+      ];
+
+      const renderedLinks = links.map((link) => {
+            return <Link 
+                        key={link.label} 
+                        to={link.path} 
+                        className='mb-3'
+                        activeClassName="font-bold border-l-4 bodred-blue-500 pl-2"
+                  >
+                        {link.label}
+                  </Link>
+      });
+
+      return (
+            <div className="sticky top-0 flex flex-col">
+                  {renderedLinks}
+            </div>
+      )
+   
+}
+
+export default Sidebar;
